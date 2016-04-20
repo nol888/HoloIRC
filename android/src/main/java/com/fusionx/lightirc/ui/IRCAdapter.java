f0c9e1775b00559293d27264728f9eb6780a4b6e
Nolan Lum <nol888@gmail.com>
2016-04-19 20:44:35 -0700

    Do things that feel bad to fix link parsing on SDK versions < Lollipop.

--- android/src/main/java/com/fusionx/lightirc/ui/IRCAdapter.java
+++ android/src/main/java/com/fusionx/lightirc/ui/IRCAdapter.java
@@ -1,5 +1,6 @@
 package com.fusionx.lightirc.ui;
 
+import android.os.Build;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 
@@ -21,6 +22,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import co.fusionx.relay.event.Event;
 
@@ -66,7 +68,12 @@ public class IRCAdapter<T extends Event> extends RecyclerView.Adapter<IRCAdapter
         }
         holder.message.setText(mEventCache.get(event).getMessage());
 
-        Linkify.addLinks(holder.message, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Linkify.addLinks(holder.message, Linkify.EMAIL_ADDRESSES);
+            Linkify.addLinks(holder.message, WEB_URL, null);
+        } else {
+            Linkify.addLinks(holder.message, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+        }
     }
 
     @Override
@@ -197,4 +204,54 @@ public class IRCAdapter<T extends Event> extends RecyclerView.Adapter<IRCAdapter
             }
         }
     }
+
+    // The following regular expressions are copied from android.util.Patterns for API versions which
+    // match against a hardcoded list of gTLDs.
+
+    /**
+     * Good characters for Internationalized Resource Identifiers (IRI).
+     * This comprises most common used Unicode characters allowed in IRI
+     * as detailed in RFC 3987.
+     * Specifically, those two byte Unicode characters are not included.
+     */
+    private static final String GOOD_IRI_CHAR =
+            "a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
+
+    private static final Pattern IP_ADDRESS
+            = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+                    + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+                    + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                    + "|[1-9][0-9]|[0-9]))");
+
+    /**
+     * RFC 1035 Section 2.3.4 limits the labels to a maximum 63 octets.
+     */
+    private static final String IRI
+            = "[" + GOOD_IRI_CHAR + "]([" + GOOD_IRI_CHAR + "\\-]{0,61}[" + GOOD_IRI_CHAR + "]){0,1}";
+
+    private static final String GOOD_GTLD_CHAR =
+            "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
+    private static final String GTLD = "[" + GOOD_GTLD_CHAR + "]{2,63}";
+    private static final String HOST_NAME = "(" + IRI + "\\.)+" + GTLD;
+
+    private static final Pattern DOMAIN_NAME
+            = Pattern.compile("(" + HOST_NAME + "|" + IP_ADDRESS + ")");
+
+    /**
+     * Regular expression pattern to match most part of RFC 3987
+     * Internationalized URLs, aka IRIs.  Commonly used Unicode characters are
+     * added.
+     */
+    private static final Pattern WEB_URL = Pattern.compile(
+            "((?:(http|https|Http|Https|rtsp|Rtsp):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
+                    + "\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_"
+                    + "\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?"
+                    + "(?:" + DOMAIN_NAME + ")"
+                    + "(?:\\:\\d{1,5})?)" // plus option port number
+                    + "(\\/(?:(?:[" + GOOD_IRI_CHAR + "\\;\\/\\?\\:\\@\\&\\=\\#\\~"  // plus option query params
+                    + "\\-\\.\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?"
+                    + "(?:\\b|$)"); // and finally, a word boundary or end of
+                                    // input.  This is to stop foo.sure from
+                                    // matching as foo.su
 }
